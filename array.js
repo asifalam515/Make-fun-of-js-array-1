aed983a44e5566e96f56a0affe535585a6feb915
asifalam515 <asibulalam515@gmail.com>
2022-07-03 22:10:06 +0600

    functional way done

---
+++ array.js
@@ -0,0 +1,11 @@
+//array is just  a collection
+//javaScript array is dynamic
+
+const arr=[];
+arr[0]=1;
+arr[1]=2;
+arr[2]=3;
+arr[99]=100;
+console.log(arr)
+
+console.log(arr.length)
---
+++ constructorArray.js
@@ -0,0 +1,7 @@
+const a1=new Array();
+console.log(a1,a1.length)
+const a2=new Array(5)
+console.log(a2,a2.length)
+
+const a3=new Array(1,2,3,4,6)
+console.log(a3)
